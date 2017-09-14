308b31a5bc30741ecfe5c80dae72c97675682c83
Spencer Childress <samussiah@gmail.com>
2017-09-14 13:58:43 -0400

    fix table variable arguments

--- src/volcano/tables/init.js
+++ src/volcano/tables/init.js
@@ -1,26 +1,42 @@
 export default function init() {
     const settings = this.parent.config;
+
     this.selected = {
         data: [],
         multiplier: 1
     };
 
     this.selected.variables = d3.merge([
-        [settings.id_col],
-        settings.structure_cols,
+        [
+            {
+                value_col: settings.id_col.value_col || settings.id_col,
+                label: settings.id_col.label || settings.id_col.value_col || settings.id_col
+            }
+        ],
+        settings.structure_cols.map(structure_col => {
+            return {
+                value_col: structure_col.value_col || structure_col,
+                label: structure_col.label || structure_col.value_col || structure_col
+            };
+        }),
         settings.detail_cols
+            ? settings.detail_cols.map(detail_col => {
+                  return {
+                      value_col: detail_col.value_col || detail_col,
+                      label: detail_col.label || detail_col.value_col || detail_col
+                  };
+              })
+            : []
     ]);
+
     this.details = {
         data: {
             details: [],
             stats: []
         }
     };
-    this.details.variables = d3.merge([
-        [settings.id_col],
-        settings.structure_cols,
-        settings.detail_cols
-    ]);
-    console.log(this);
+
+    this.details.variables = this.selected.variables;
+
     this.layout();
 }
